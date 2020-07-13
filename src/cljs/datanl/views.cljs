c4483fd8a0e03639e486faf8fa62fddccd0a9f9f
Adam Bergman <adambergman@live.se>
2020-07-13 18:21:14 +0200

    dummy site

--- src/cljs/datanl/views.cljs
+++ src/cljs/datanl/views.cljs
@@ -5,7 +5,8 @@
              [stylefy.core :refer [use-style]]
              [reagent.core :as reagent]
              [goog.crypt.base64 :as b64]
-             [ajax.protocols :refer [-body]]))
+             [ajax.protocols :refer [-body]]
+             ["faker" :as faker]))
 
 (defn label-input []
   (let [label-field-value (re-frame/subscribe [::subs/field-value])] ;; @(re-frame/subscribe [::forms/field-value form-id label-field-path])]
@@ -21,6 +22,7 @@
 
 (defn handler [response]
   (reset! result response))  ;(js->clj response :keywordize-keys true)))
+
 
 
 (defn error-handler [error]
@@ -45,18 +47,54 @@
   [:div.card.col-4
    [:img.card-img-top {:alt "Card image cap", :src (str "https://picsum.photos/400?random=" img-n)}]
    [:div.card-body
-    [:h5.card-title "Card title"]
+    [:h5.card-title (.companyName (.-company faker))]
    ; [:h6.card-subtitle.mb-2.text-muted "Card subtitle"]
-    [:p.card-text
-     "Some quick example text to build on the card title and make up the bulk of the card's content."]
-    [:a.card-link {:href "#"} "Card link"]
-    [:a.card-link {:href "#"} "Another link"]]])
+    [:p.card-text (.catchPhrase (.-company faker))]
+    [:a.card-link {:href "#"} (.jobTitle (.-name faker))]]])
+    ;[:a.card-link {:href "#"} "Another link"]]])
 
 (defn get-random-images []
   [:div {:class "row align-items-center"}
     (map card2 (range 9))])
+[:img {:src "images/logo.png" :class "header-itema" :style {:max-width "25%"}}]
+(defn navbar []
+  [:nav.navbar.navbar-expand-lg.navbar-light.bg-light
+   [:a.navbar-brand
+    {:href "#"}
+    [:img.d-inline-block.align-top
+     {:loading "lazy"
+      :alt ""
+      
+      :width "200"
+      :src "images/logo.png"}]]
+   [:button.navbar-toggler
+    {:aria-label "Toggle navigation"
+     :aria-expanded "false"
+     :aria-controls "navbarText"
+     :data-target "#navbarText"
+     :data-toggle "collapse"
+     :type "button"}
+    [:span.navbar-toggler-icon]]
+   [:div#navbarText.collapse.navbar-collapse
+    [:ul.navbar-nav.mr-auto
+     [:li.nav-item.active
+      [:a.nav-link {:href "#"} "Home " [:span.sr-only "(current)"]]]
+     [:li.nav-item [:a.nav-link {:href "#"} "Features"]]
+     [:li.nav-item [:a.nav-link {:href "#"} "Pricing"]]]
+    [:form.form-inline
+     [:input.form-control.mr-sm-2
+      {:aria-label "Search", :placeholder "Search", :type "search"}]
+     [:button.btn.btn-outline-success.my-2.my-sm-0
+      {:type "submit"}
+      "Search"]]]])
 
 
+(defn jumbo []
+  [:div.jumbotron.jumbotron-fluid
+    [:div.container
+     [:h1.display-4 (.catchPhrase (.-company faker))]
+     [:p.lead
+      "This sector has innovative wheelhouse. Skate to where the puck is going to be."]]])
 
 (def button-style {:padding "25px"
                    :background-color "#fcb603"
@@ -75,14 +113,18 @@
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/field-value])]
-    [:div {:class "jumbotron container"}
+    
+    [:div {:class "container"}
+     
      (comment [photo-grid])
+     [navbar]
+     [jumbo]
+     ;[counter-button]
+     [:div {:class "flex" :style {:display "flex" :flex-direction "row" :justify-content "space-between"}}]
+      
 
-     [counter-button]
-     [:div {:class "flex" :style {:display "flex" :flex-direction "row" :justify-content "space-between"}}
-      [:img {:src "images/logo.png" :class "header-itema" :style {:max-width "25%"}}]
-      [:div {:class "header-item" :style {:text-color "green"}} "Start page"]
-      [:div {:class "header-item"} "Settings"]
-      [:div {:class "myButton"} "My Cart"]]
      [:div
       [get-random-images]]]))
+        
+ 
+ 
